3912cc5dade3597e139c1cb3dd9bd8a6999f1d49
HerveDavid <david.herve99@gmail.com>
2022-05-31 10:57:38 +0200

    feat(style): integrate TailwindCss
    For fastest development, add tailwind plugin
    
    Fixes #4

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './pages/booking/booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BookingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
