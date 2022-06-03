48eb99accc0d79587c9d52fd543259d4afa26f84
HerveDavid <david.herve99@gmail.com>
2022-06-03 15:18:55 +0200

    feat(bdd): add complete availabilities room
    configuration.
    
    fixes #0

--- backend/src/main/java/com/escape_the_world/app/EscapeTheWorldApplication.java
+++ backend/src/main/java/com/escape_the_world/app/EscapeTheWorldApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Configuration
+@ConfigurationPropertiesScan("com.escape_the_world.configurations")
 @ComponentScan(basePackages = {"com.escape_the_world"})
 @EntityScan("com.escape_the_world.entities")
 @EnableJpaRepositories("com.escape_the_world.repositories")
