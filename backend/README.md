c1fad847d856e856535f3b1e86031e0debbee208
HerveDavid <david.herve99@gmail.com>
2022-06-19 10:39:42 +0200

    fix(doc): add minimal doc and fix launching
    fixes #4

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,17 @@
 # Escape the world
+
+## Running application
+
+To launch this application, run :
+```
+mvn clean install
+java -Dfile.encoding=UTF-8 -jar target/EscapeTheWorld-0.0.1-SNAPSHOT.jar
+```
+
+## Testing application
+
+To test this application, run :
+
+```
+mvn clean test
+```
--- backend/src/main/java/com/escape_the_world/repositories/UnavailabilityRepository.java
+++ backend/src/main/java/com/escape_the_world/repositories/UnavailabilityRepository.java
@@ -2,15 +2,8 @@ package com.escape_the_world.repositories;
 
 import com.escape_the_world.entities.Unavailability;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
 
 @Repository
 public interface UnavailabilityRepository extends JpaRepository<Unavailability, String> {
-
-    @Query("Select u from Unavailability u where u.idRoom === ?1")
-    ArrayList<Unavailability> getUnavailabiltiesByRoom(String idRoom);
-
 }
