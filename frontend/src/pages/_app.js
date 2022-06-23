19504c2a7a7e27ff315158347b98589a47fe8c07
david_herve <david.herve@rte-france.com>
2022-06-23 11:26:52 +0200

    feat(index) add a homepage

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -18,7 +18,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Material Kit Pro
+          Escape the World
         </title>
         <meta
           name="viewport"
--- frontend/src/utils/get-format-date.js
+++ frontend/src/utils/get-format-date.js
@@ -1,12 +1,8 @@
-function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
+const padTo2Digits = (num) => (num.toString().padStart(2, "0"))
 
-export const  getFormatDate = (num) => {
+export const getFormatDate = (num) => {
   const date = new Date(num);
-  return [
-    padTo2Digits(date.getDate()),
-    padTo2Digits(date.getMonth() + 1),
-    date.getFullYear(),
-  ].join('/');
-}
+  return [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join(
+    "/"
+  );
+};
